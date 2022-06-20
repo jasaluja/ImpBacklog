70dae0d7cf493a455d97c1c9975458fc3d2e4984
HussainFiserv <107839246+HussainFiserv@users.noreply.github.com>
2022-06-20 01:22:42 -0700

    Created appsettings.js

---
+++ Backlog Segregation Tool/appsettings.js
@@ -0,0 +1,11 @@
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft": "Warning",
+      "Microsoft.Hosting.Lifetime": "Information"
+    }
+  },
+  "AllowedHosts": "*",
+  "Excel_path": "C:\\Users\\F91VVIU\\Downloads\\testexcel.xlsx"
+}
