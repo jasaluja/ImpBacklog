4159e31677ab5d90a849ebf3adc1e8100a4b8d02
hussainFiserv <hussainali.bohra@fiserv.com>
2022-06-23 04:12:02 -0700

    development 06-23

---
+++ Backlog Segregation Tool/BusinessLogic/Utility.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+namespace Backlog_Segregation_Tool.BusinessLogic
+{
+	public static class Utility
+	{
+		public static DataTable ReOrderTable(DataTable table, String[] columnNames)
+		{
+			int columnIndex = 0;
+			foreach (var columnName in columnNames)
+			{
+				table.Columns[columnName].SetOrdinal(columnIndex);
+				columnIndex++;
+			}
+			int i = columnIndex;
+			while (i < table.Columns.Count)
+			{
+				table.Columns.RemoveAt(i);
+			}
+			return table;
+		}
+
+		public static DataTable AddPreTriageStatusColumn(DataTable backlog)
+		{
+			backlog.Columns.Add("PreTriageStatus", typeof(bool));
+			foreach (var data in backlog.AsEnumerable())
+			{
+				String tag = data.Field<String>("Tag");
+				if (tag != null)
+				{
+					if (tag.ToLower().Contains("PretriageAccepted".ToLower()))
+					{
+						data.BeginEdit();
+						data["PreTriageStatus"] = true;
+						data.EndEdit();
+					}
+					else
+					{
+						data.BeginEdit();
+						data["PreTriageStatus"] = false;
+						data.EndEdit();
+					}
+
+				}
+				else
+				{
+					data.BeginEdit();
+					data["PreTriageStatus"] = false;
+					data.EndEdit();
+				}
+			}
+			return backlog;
+		}
+	}
+}
--- Backlog Segregation Tool/Controllers/HomeController.cs
+++ Backlog Segregation Tool/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using System.Data;
 using Backlog_Segregation_Tool.Models;
+using Backlog_Segregation_Tool.BusinessLogic;
 
 namespace Backlog_Segregation_Tool.Controllers
 {
@@ -17,23 +18,30 @@ namespace Backlog_Segregation_Tool.Controllers
 		private readonly IConfiguration _config;
 		public DataSet backlog = new DataSet();
 		public BacklogDataModel backlogData = new BacklogDataModel();
-	    
+
+		public String[] ColumnsOrder;
 		public String[] diplometsClients;
+		public String ChallangerTag;
+		public int ChallangersMaxDays;
 		public HomeController(IConfiguration configuration)
 		{
 			_config = configuration;
 			diplometsClients = _config.GetSection("DiplometsClients").Get<List<string>>().ToArray();
+			ChallangerTag = _config.GetSection("ChallangerTag").Value.ToString();
+			ChallangersMaxDays =Convert.ToInt32(_config.GetSection("ChallangersMaxDays").Value.ToString());
+			ColumnsOrder = _config.GetSection("ColumnsOrder").Get<List<string>>().ToArray();
 		}
 
-		public IActionResult Index(String gname= "DCHA.3.Architect.AMS.Imp")
+		public IActionResult Index(String gname = "DCHA.3.Architect.AMS.Imp")
 		{
-			Console.WriteLine("excel readerjhgkhk");
 			var path = _config.GetSection("Excel_path").Value;
 			ExcelReader excelReader = new ExcelReader(path,gname);
 			DataTable fdata = excelReader.FilteredData;
+			fdata = Utility.ReOrderTable(fdata,ColumnsOrder);
+			fdata = Utility.AddPreTriageStatusColumn(fdata);
 			BacklogSperator bs = new BacklogSperator(fdata);
 			bs.getDiplomatsCases(diplometsClients);
-			bs.getChallangersCases(36);
+			bs.getChallangersCases(ChallangersMaxDays,ChallangerTag);
 			backlogData = bs.getSepratedBacklog();
 			if (gname == "DCHA.3.Architect.AMS.Imp")
 			{
